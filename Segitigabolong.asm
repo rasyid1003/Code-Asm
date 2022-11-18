8b5d28b4160405e67dd25fe62fb17a18ed27482c
Faris Rasyid <85282829+rasyid1003@users.noreply.github.com>
2022-11-18 10:07:18 +0700

    Create Segitigabolong.asm

---
+++ Segitigabolong.asm
@@ -0,0 +1,60 @@
+.MODEL SMALL
+.code
+org 100h
+proses:
+        MOV AH,02h
+        MOV DL,''
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL, 0ah
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,''
+        MOV DL, 0ah
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,''
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL, 0ah
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+        MOV DL,'*'
+        INT 21h
+
+
+        INT 20h
+END PROSES
